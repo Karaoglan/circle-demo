fb6c96b4ef48644548a6be33e781ae58dffa0799
Burak Karaoglan <burakkaraoglan55@gmail.com>
2022-06-03 10:56:46 +0300

    add test and circle config

---
+++ test/calc.test.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+
+describe("TEST MAIN", function() {
+    describe("TEST SUB", function() {
+      it("check if pass", function() {
+  
+        expect(true).to.equal(true);
+      });
+      it("check if not pass", function() {
+  
+        expect(true).to.equal(false);
+      });
+    });
+  
+  });
